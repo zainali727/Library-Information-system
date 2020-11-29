b1e23b7b5802f1d1b04485ec5240f597918c500d
zaa <zaa@DESKTOP-6BMON61>
2020-11-29 14:54:42 +0000

    adding books

---
+++ WebApplication/WebApplication/Controllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Data;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            var books= _context.books.ToList();
+            return View(Books);
+        }
+
+        // GET: Customer/Create
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            return View(id == 0
+                ? new books()
+                : _context.Books.Find(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                if (book.Id == 0)
+                    _context.Add(book);
+                else
+                    _context.Update(book);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var books = await _context.Books.FindAsync(id);
+
+            if (books != null)
+            {
+                _context.Books.Remove(book);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Models/Books.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication.Models
+{
+    public class Books
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        public string Title { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        public string Author { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        public string  ISBN { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        public string  Genre { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        public string  PublishedDate { get; set; }
+    }
+}
