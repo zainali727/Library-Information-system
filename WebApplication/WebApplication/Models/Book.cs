29598b97e1f38b50170c880ae9630a4d84a3cb5f
zaa <zaa@DESKTOP-6BMON61>
2020-11-29 15:52:40 +0000

    Validation

--- WebApplication/WebApplication/Models/Book.cs
+++ WebApplication/WebApplication/Models/Book.cs
@@ -8,14 +8,19 @@ namespace WebApplication.Models
         [Key]
         public int Id { get; set; }
         [Column(TypeName ="nvarchar(250)")]
+        [Required]
         public string Title { get; set; }
         [Column(TypeName ="nvarchar(250)")]
+        [Required]
         public string Author { get; set; }
         [Column(TypeName ="nvarchar(250)")]
+        [Required]
         public string  ISBN { get; set; }
         [Column(TypeName ="nvarchar(250)")]
+        [Required]
         public string  Genre { get; set; }
         [Column(TypeName ="nvarchar(250)")]
+        [Required]
         public string  PublishedDate { get; set; }
     }
 }
--- WebApplication/WebApplication/Models/Customer.cs
+++ WebApplication/WebApplication/Models/Customer.cs
@@ -7,15 +7,21 @@ namespace WebApplication.Models
     {
         [Key]
         public int Id { get; set; }
-        [Column(TypeName ="nvarchar(250)")]
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
         public string Firstname { get; set; }
-        [Column(TypeName ="nvarchar(250)")]
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
         public string Lastname { get; set; }
-        [Column(TypeName ="nvarchar(250)")]
-        public string  Address { get; set; }
-        [Column(TypeName ="nvarchar(250)")]
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        public string Address { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        [EmailAddress, Required]
+
         public string  Email { get; set; }
         [Column(TypeName ="nvarchar(250)")]
+        [Required]
         public string  Telephone { get; set; }
     }
 }
