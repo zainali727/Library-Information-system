41d9b3d50267aa1ab76ac00cff565efb0fdaa9d7
zaa <zaa@DESKTOP-6BMON61>
2020-12-06 16:05:50 +0000

    Added Address fields

---
+++ WebApplication/WebApplication/Data/Migrations/20201206155839_Address_fields.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Data.Migrations
+{
+    public partial class Address_fields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Customers",
+                newName: "PostCode");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine1",
+                table: "Customers",
+                type: "nvarchar(250)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine2",
+                table: "Customers",
+                type: "nvarchar(250)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine3",
+                table: "Customers",
+                type: "nvarchar(250)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Customers",
+                type: "nvarchar(250)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "County",
+                table: "Customers",
+                type: "nvarchar(250)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressLine1",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "AddressLine2",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "AddressLine3",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "County",
+                table: "Customers");
+
+            migrationBuilder.RenameColumn(
+                name: "PostCode",
+                table: "Customers",
+                newName: "Address");
+        }
+    }
+}
--- WebApplication/WebApplication/Models/Customer.cs
+++ WebApplication/WebApplication/Models/Customer.cs
@@ -7,19 +7,44 @@ namespace WebApplication.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Column(TypeName = "nvarchar(250)")]
         [Required]
+        [MaxLength (25)]
         public string Firstname { get; set; }
+
         [Column(TypeName = "nvarchar(250)")]
         [Required]
         public string Lastname { get; set; }
+
         [Column(TypeName = "nvarchar(250)")]
         [Required]
-        public string Address { get; set; }
+        public string AddressLine1{ get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        public string AddressLine2 { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        public string AddressLine3 { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        public string City { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        public string County { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        [Required]
+        public string PostCode { get; set; }
+
         [Column(TypeName = "nvarchar(250)")]
         [EmailAddress, Required]
-
         public string  Email { get; set; }
+
         [Column(TypeName ="nvarchar(250)")]
         [Required]
         public string  Telephone { get; set; }
